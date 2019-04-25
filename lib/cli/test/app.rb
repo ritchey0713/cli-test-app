1ea6a42b6e50425853a404f6a236d7342649b82b
ritchey0713 <ritchey0713@gmail.com>
2019-04-25 09:07:47 -0400

    first commit

---
+++ lib/cli/test/app.rb
@@ -0,0 +1,10 @@
+require "cli/test/app/version"
+
+module Cli
+  module Test
+    module App
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/cli/test/app/version.rb
@@ -0,0 +1,7 @@
+module Cli
+  module Test
+    module App
+      VERSION = "0.1.0"
+    end
+  end
+end
